769a06c19b93ce449e003fef67e1660e39959e2d
Viktor-Kl <viktorkyrylovych88@gmail.com>
2023-04-09 09:09:03 +0300

    Create head of userCard

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,19 +1,30 @@
-import { FC } from 'react'
-import styles from './UserCard.module.scss'
-import { LocalGithubUser } from '../../types'
+import { FC } from "react";
+import styles from "./UserCard.module.scss";
+import { LocalGithubUser } from "../../types";
+import { UserStat } from "../UserStat/UserStat";
+import UserTitle from "../UserTitle/";
 
 interface UserCardProps extends LocalGithubUser {}
 
 export const UserCard: FC<UserCardProps> = (props) => {
-    return (
-        <div className={styles.userCard}>
-            <UserStat 
-                repos={props.repos}
-                followers={props.followers}
-                following={props.following}
-            />
-        </div>
-    )
-}
+  return (
+    <div className={styles.userCard}>
+      <img src={props.avatar} alt={props.login} className={styles.avatart} />
+      <UserTitle
+        created={props.created}
+        login={props.login}
+        name={props.name}
+      />
+      <p className={`${styles.bio}${!props.bio ? '' : ` ${styles.empty}`}`}>
+        {props.bio || 'This profile has no bio'}
+      </p>
+      <UserStat
+        repos={props.repos}
+        followers={props.followers}
+        following={props.following}
+      />
+    </div>
+  );
+};
 
 export default UserCard;
